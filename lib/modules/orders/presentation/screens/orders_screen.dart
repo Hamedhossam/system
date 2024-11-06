6eabe3ac28f0fb1c248c392ed7fe58991c796819
Hamedhossam <hamedhossamelmarasy@gmail.com>
2024-11-06 18:56:16 +0200

    adding orders screen

--- lib/modules/orders/presentation/screens/orders_screen.dart
+++ lib/modules/orders/presentation/screens/orders_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:motors/core/widgets/horizental_line.dart';
+import 'package:motors/core/widgets/label.dart';
+import 'package:motors/core/widgets/text_field.dart';
+import 'package:motors/modules/orders/presentation/widgets/all_orders.dart';
+import 'package:motors/modules/orders/presentation/widgets/last_week_orders.dart';
+import 'package:motors/modules/orders/presentation/widgets/orders_screen_tittle.dart';
+import 'package:motors/modules/orders/presentation/widgets/today_orders.dart';
+import 'package:motors/modules/orders/presentation/widgets/yesterday_orders.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
@@ -7,7 +15,50 @@ class OrdersScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.white,
-      body: Center(child: Text('Setting View')),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            OrdersScreenTittle(),
+            HorizentalLine(),
+            OrdersLabel(tittle: 'Today'),
+            TodayOrders(),
+            HorizentalLine(),
+            OrdersLabel(tittle: 'Yesterday'),
+            YesterdayOrders(),
+            HorizentalLine(),
+            OrdersLabel(tittle: 'Last Week'),
+            LastWeekOrders(),
+            HorizentalLine(),
+            OrdersLabel(tittle: 'All'),
+            AllOrders(),
+            HorizentalLine()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OrdersLabel extends StatelessWidget {
+  const OrdersLabel({
+    super.key,
+    required this.tittle,
+  });
+  final String tittle;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(children: [
+        SizedBox(width: 150, child: Label(tittle: tittle)),
+        const Padding(
+          padding: EdgeInsets.only(left: 8.0),
+          child: SizedBox(
+            width: 200,
+            child: CustomizedTextField(tittle: "Search with id", maxLines: 1),
+          ),
+        )
+      ]),
     );
   }
 }
---
+++ lib/modules/orders/presentation/widgets/last_week_orders.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+class LastWeekOrders extends StatelessWidget {
+  const LastWeekOrders({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 210,
+          width: 1450,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 0, 0, 0), width: 2),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "#Products",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 160,
+                              width: 200,
+                              child: ListView.builder(
+                                itemCount: 10,
+                                itemBuilder: (context, index) {
+                                  return const Text(
+                                    "2 x Adiddas Super Star",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(255, 92, 91, 91)),
+                                  );
+                                },
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "#Details",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            const Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(right: 5.0),
+                                  child: Icon(Icons.qr_code),
+                                ),
+                                Text(
+                                  "302015",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 92, 91, 91)),
+                                ),
+                              ],
+                            ),
+                            const Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(right: 5.0),
+                                  child: Icon(Icons.person),
+                                ),
+                                Text(
+                                  "hamed hossam eldin",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 92, 91, 91)),
+                                ),
+                              ],
+                            ),
+                            const Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(right: 5.0),
+                                  child: Icon(Icons.phone_android),
+                                ),
+                                Text(
+                                  "01063876967",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 92, 91, 91)),
+                                ),
+                              ],
+                            ),
+                            const Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(right: 5.0),
+                                  child: Icon(Icons.timer),
+                                ),
+                                Text(
+                                  "16/9/2002 03:14 AM",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 92, 91, 91)),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              children: [
+                                ElevatedButton(
+                                  style: const ButtonStyle(
+                                    backgroundColor:
+                                        WidgetStatePropertyAll(Colors.blue),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Icon(
+                                    Icons.edit,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                ElevatedButton(
+                                    style: const ButtonStyle(
+                                      backgroundColor:
+                                          WidgetStatePropertyAll(Colors.red),
+                                    ),
+                                    onPressed: () {},
+                                    child: const Icon(
+                                      Icons.replay_outlined,
+                                      color: Colors.white,
+                                    )),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
